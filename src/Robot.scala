f13b5b1f6b23f58170f32450912545d30817da44
YanDoroshenko <yan1994doroshenko@gmail.com>
2016-11-23 21:37:52 +0100

    Relatively functional version

---
+++ src/Robot.scala
@@ -0,0 +1,174 @@
+import java.io.FileOutputStream
+import java.net.{ServerSocket, Socket}
+import java.util.concurrent.Executors
+
+import scala.annotation.tailrec
+
+/**
+  * Created by Yan Doroshenko (yandoroshenko@protonmail.com) on 23.11.16.
+  */
+object Robot {
+
+  class Communicator(socket: Socket, connectionId: Int) extends Thread {
+    final val FOTO = "FOTO "
+    final val INFO = "INFO "
+
+    val reader = socket.getInputStream()
+    val writer = socket.getOutputStream()
+    val logger = new FileOutputStream("Robot" + connectionId + ".log")
+
+    def login = {
+      writer write "200 LOGIN\r\n".getBytes
+      logger write "Server: Login?\r\n".getBytes
+    }
+
+    def passwd = {
+      writer write "201 PASSWORD\r\n".getBytes
+      logger write "Server: Password?\r\n".getBytes
+    }
+
+    def ok = {
+      writer write "202 OK\r\n".getBytes
+      logger write "Server: OK\r\n".getBytes
+    }
+
+    def badChecksum = {
+      writer write "300 BAD CHECKSUM\r\n".getBytes
+      logger write "Server: Bad checksum\r\n".getBytes
+    }
+
+    def loginFailed = {
+      logger write "Server: Login failed\r\n".getBytes
+      if (!socket.isClosed) {
+        writer write "500 LOGIN FAILED\r\n".getBytes
+        socket.close()
+      }
+    }
+
+    def syntaxError = {
+      logger write "Server: Syntax error\r\n".getBytes
+      if (!socket.isClosed) {
+        writer write "501 SYNTAX ERROR\r\n".getBytes
+        socket.close()
+      }
+    }
+
+    def timeout = {
+      logger write "Server: Timeout\r\n".getBytes
+      if (!socket.isClosed) {
+        writer write "502 TIMEOUT\r\n".getBytes
+        socket.close()
+      }
+    }
+
+    def read = {
+
+      @tailrec
+      def readRec(last: Char, buffer: StringBuilder): String = {
+        val c = reader.read().toChar
+        if (last == '\r' && c == '\n')
+          buffer.toString()
+        else
+          readRec(c,
+            if (last == '\u0000')
+              buffer
+            else
+              buffer + last
+          )
+      }
+
+      val res = readRec('\u0000', new StringBuilder)
+      logger write ("Client: " + res + "\r\n").getBytes()
+      res
+    }
+
+    def readSyntax = {
+
+      @tailrec
+      def readRec(last: Char, buffer: StringBuilder): String = {
+        val c = reader.read().toChar
+        buffer += c
+        val str = buffer.toString
+        str length match {
+          case l if l < 6 && str != (FOTO take l) && str != (INFO take l) =>
+            syntaxError
+            buffer.toString()
+          case 6 if (str take 5) == (FOTO take 5) && (str(5) < '0' || str(5) > '9') =>
+            syntaxError
+            buffer.toString()
+          case _ if (str take 5) == FOTO && (str split " ").length == 2 && str.last == ' ' =>
+            val length = (str split " ") (1).toInt
+            val foto = readFoto(new StringBuilder, 0, length)
+            val hashsum = (reader.read() << 24) |
+              (reader.read() << 16) |
+              (reader.read() << 8) |
+              reader.read()
+            if (foto.map(_.toLong).sum == hashsum)
+              ok
+            else
+              badChecksum
+            str + foto.mkString("["," ", "]") + " with actual hashsum: " + foto.map(_.toLong).sum + "\r\nHashsum entered: " + hashsum
+          case _ if last == '\r' && c == '\n' =>
+            ok
+            buffer.toString()
+          case _ =>
+            readRec(c, buffer)
+        }
+      }
+
+      @tailrec
+      def readFoto(buffer: StringBuilder, read: Long, length: Long): Array[Byte] = {
+        if (read == length)
+          buffer.toArray.map(_.toByte)
+        else {
+          readFoto(buffer + reader.read().toChar, read + 1, length)
+        }
+      }
+
+      val res = readRec('\u0000', new StringBuilder)
+      logger write ("Client: " + res + "\r\n").getBytes()
+    }
+
+    override def run(): Unit = {
+      new Thread() {
+        override def run(): Unit = {
+          Thread.sleep(45000)
+          timeout
+        }
+      }.start()
+
+      login
+      val username = read
+      passwd
+      val password = try {
+        read.toLong
+      }
+      catch {
+        case _: NumberFormatException =>
+          -1
+      }
+      if (username.toCharArray.map(_.toLong).sum == password)
+        ok
+      else
+        loginFailed
+
+      while (!socket.isClosed)
+        readSyntax
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    if (args.length < 1)
+      println("No port specified!")
+    else {
+      print("Creating server on port " + args(0))
+      val socket = new ServerSocket(args(0).toInt)
+      val executor = Executors.newCachedThreadPool()
+      var i = 1
+      while (!socket.isClosed) {
+        executor submit new Communicator(socket.accept(), i)
+        i += 1
+      }
+    }
+  }
+}
---
+++ src/Testing.java
@@ -0,0 +1,224 @@
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.SocketException;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Yan Doroshenko (yandoroshenko@protonmail.com) on 23.11.16.
+ */
+public class Testing {
+
+    private static Thread server;
+
+    @BeforeClass
+    public static void startServer() throws Exception {
+        String[] args = {"3999"};
+        server = new Thread(() -> Robot.main(args));
+        server.start();
+        Thread.sleep(6000);
+    }
+
+    @Test
+    public void testSyntaxError() throws Exception {
+        Socket socket = new Socket("127.0.0.1", 3999);
+        OutputStream out = socket.getOutputStream();
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        assertEquals("200 LOGIN", in.readLine());
+        out.write("2\r\n".getBytes());
+        out.flush();
+        assertEquals("201 PASSWORD", in.readLine());
+        out.write("50\r\n".getBytes());
+        out.flush();
+        assertEquals("202 OK", in.readLine());
+        out.write("2\r\n".getBytes());
+        out.flush();
+        assertEquals("501 SYNTAX ERROR", in.readLine());
+        try {
+            assertEquals(-1, in.read());
+        }
+        catch (SocketException e) {
+        }
+        socket.close();
+    }
+
+    @Test
+    public void testInfo() throws Exception {
+        Socket socket = new Socket("127.0.0.1", 3999);
+        OutputStream out = socket.getOutputStream();
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        assertEquals("200 LOGIN", in.readLine());
+        out.write("2\r\n".getBytes());
+        out.flush();
+        assertEquals("201 PASSWORD", in.readLine());
+        out.write("50\r\n".getBytes());
+        out.flush();
+        assertEquals("202 OK", in.readLine());
+        out.write("INFO ABCDE\r\n".getBytes());
+        out.flush();
+        assertEquals("202 OK", in.readLine());
+        out.write("INFOABCDE\r\n".getBytes());
+        out.flush();
+        assertEquals("501 SYNTAX ERROR", in.readLine());
+        try {
+            assertEquals(-1, in.read());
+        }
+        catch (SocketException e) {
+        }
+        socket.close();
+    }
+
+    @Test
+    public void testLoginFailed() throws Exception {
+        Socket socket = new Socket("127.0.0.1", 3999);
+        OutputStream out = socket.getOutputStream();
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        assertEquals("200 LOGIN", in.readLine());
+        out.write("2\r\n".getBytes());
+        out.flush();
+        assertEquals("201 PASSWORD", in.readLine());
+        out.write("51\r\n".getBytes());
+        out.flush();
+        assertEquals("500 LOGIN FAILED", in.readLine());
+        try {
+            assertEquals(-1, in.read());
+        }
+        catch (SocketException e) {
+        }
+        socket.close();
+    }
+
+    @Test
+    public void testFoto() throws Exception {
+        Socket socket = new Socket("127.0.0.1", 3999);
+        OutputStream out = socket.getOutputStream();
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        assertEquals("200 LOGIN", in.readLine());
+        out.write("2\r\n".getBytes());
+        out.flush();
+        assertEquals("201 PASSWORD", in.readLine());
+        out.write("50\r\n".getBytes());
+        out.flush();
+        assertEquals("202 OK", in.readLine());
+        out.write("FOTO 8 ABCDEFGH".getBytes());
+        byte[] hashsum = {0x0, 0x0, 0x2, 0x24};
+        out.write(hashsum);
+        out.flush();
+        assertEquals("202 OK", in.readLine());
+        out.write("50\r\n".getBytes());
+        out.flush();
+        assertEquals("501 SYNTAX ERROR", in.readLine());
+        try {
+            assertEquals(-1, in.read());
+        }
+        catch (SocketException e) {
+        }
+        socket.close();
+    }
+
+    @Test
+    public void testMalformedFoto() throws Exception {
+        Socket socket = new Socket("127.0.0.1", 3999);
+        OutputStream out = socket.getOutputStream();
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        assertEquals("200 LOGIN", in.readLine());
+        out.write("2\r\n".getBytes());
+        out.flush();
+        assertEquals("201 PASSWORD", in.readLine());
+        out.write("50\r\n".getBytes());
+        out.flush();
+        assertEquals("202 OK", in.readLine());
+        out.write("FOTO 13 A\r\nB\0CD\r\nEFGH".getBytes());
+        byte[] hashsum = {0x0, 0x0, 0x2, 0x52};
+        out.write(hashsum);
+        out.flush();
+        assertEquals("202 OK", in.readLine());
+        out.write("50\r\n".getBytes());
+        out.flush();
+        assertEquals("501 SYNTAX ERROR", in.readLine());
+        try {
+            assertEquals(-1, in.read());
+        }
+        catch (SocketException e) {
+        }
+        socket.close();
+    }
+
+    @Test
+    public void testBadChecksum() throws Exception {
+        Socket socket = new Socket("127.0.0.1", 3999);
+        OutputStream out = socket.getOutputStream();
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        assertEquals("200 LOGIN", in.readLine());
+        out.write("2\r\n".getBytes());
+        out.flush();
+        assertEquals("201 PASSWORD", in.readLine());
+        out.write("50\r\n".getBytes());
+        out.flush();
+        assertEquals("202 OK", in.readLine());
+        out.write("FOTO 8 ABCDEFGH".getBytes());
+        byte[] hashsum = {0x0, 0x0, 0x2, 0x23};
+        out.write(hashsum);
+        out.flush();
+        assertEquals("300 BAD CHECKSUM", in.readLine());
+        out.write("50\r\n".getBytes());
+        out.flush();
+        assertEquals("501 SYNTAX ERROR", in.readLine());
+        try {
+            assertEquals(-1, in.read());
+        }
+        catch (SocketException e) {
+        }
+        socket.close();
+    }
+
+    @Test
+    public void testComplexFoto() throws Exception {
+        Socket socket = new Socket("127.0.0.1", 3999);
+        OutputStream out = socket.getOutputStream();
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        assertEquals("200 LOGIN", in.readLine());
+        out.write("2\r\n".getBytes());
+        out.flush();
+        assertEquals("201 PASSWORD", in.readLine());
+        out.write("50\r\n".getBytes());
+        out.flush();
+        assertEquals("202 OK", in.readLine());
+        out.write("FOTO 8 ABCDEFGH".getBytes());
+        byte[] hashsum = {0x0, 0x0, 0x2, 0x24};
+        out.write(hashsum);
+        out.flush();
+        assertEquals("202 OK", in.readLine());
+        out.write("FOTO 8 ABCDEFGH".getBytes());
+        byte[] hashsum1 = {0x0, 0x0, 0x2, 0x23};
+        out.write(hashsum1);
+        out.flush();
+        assertEquals("300 BAD CHECKSUM", in.readLine());
+        out.write("FOTO 8 ABCDEFGH".getBytes());
+        byte[] hashsum2 = {0x0, 0x0, 0x2, 0x24};
+        out.write(hashsum2);
+        out.flush();
+        assertEquals("202 OK", in.readLine());
+        out.write("50\r\n".getBytes());
+        out.flush();
+        assertEquals("501 SYNTAX ERROR", in.readLine());
+        try {
+            assertEquals(-1, in.read());
+        }
+        catch (SocketException e) {
+        }
+        socket.close();
+    }
+
+    @AfterClass
+    public static void stopServer() throws Exception {
+        server.interrupt();
+    }
+
+}
